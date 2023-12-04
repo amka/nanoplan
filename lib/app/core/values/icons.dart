3682e5ecd6a8d3435eae88fd51cfc374ae5ad3a0
Andrey Maksimov <andrey@tenderowl.com>
2023-12-04 19:06:29 +0300

    Add app constants

---
+++ lib/app/core/values/icons.dart
@@ -0,0 +1,6 @@
+const personIcon = 0xed91; // perso.
+const workIcon = 0xe11c; // work
+const movieIcon = 0xed0f; // movie
+const sportIcon = 0xeddc; // sport
+const travelIcon = 0xed71; // travel
+const shopIcon = 0xe59c; // shop
