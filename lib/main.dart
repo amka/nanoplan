b0d7c4055937185ebba4079a481eaf3f76a0fe0e
Andrey Maksimov <andrey@tenderowl.com>
2023-12-04 19:21:00 +0300

    Update app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:nanoplan/app/data/services/storage/service.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import 'app/data/services/storage/service.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
@@ -12,7 +13,12 @@ void main() async {
 
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "NanoPlan",
+      theme: ThemeData(
+        colorSchemeSeed: Colors.pink.shade600,
+        useMaterial3: true,
+        textTheme: GoogleFonts.notoSansTextTheme(),
+      ),
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
